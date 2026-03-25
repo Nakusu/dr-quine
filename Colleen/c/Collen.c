dc2754aeaabfdb82a130a780279efe33a3794c49
Leopold LEPAGE <llepage@f6r2s15.clusters.42paris.fr>
2026-03-25 15:11:59 +0100

    [collen] cleaning

--- Colleen/c/Collen.c
+++ Colleen/c/Collen.c
@@ -3,7 +3,7 @@
 // Hello ;)
 
 char *getContent() {
-	return "#include <stdio.h>%1$c%1$c// Hello ;)%1$c%1$cchar *getContent() {%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint main() {%1$c%2$c// Hi there :)%1$c%1$c    char *content = getContent();%1$c%2$cprintf(content, 10, 9, 34, content);%1$c}%1$c";
+	return "#include <stdio.h>%1$c%1$c// Hello ;)%1$c%1$cchar *getContent() {%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint main() {%1$c%2$c// Hi there :)%1$c%1$c    char *content = getContent();%1$c%2$cprintf(content, 10, 9, 34, content);%1$c}";
 }
 
 int main() {
