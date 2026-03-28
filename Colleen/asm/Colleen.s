8adb8698c5ad665c4dc0387c430ed968cbf4dacb
Nakusu <nakusu35@hotmail.fr>
2026-03-28 15:36:07 +0100

    [colleen] adapt with subject

--- Colleen/asm/Colleen.s
+++ Colleen/asm/Colleen.s
@@ -1,17 +1,22 @@
 extern printf
 
 section .data
-	msg db "extern printf%1$c%1$csection .data%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$csection .text%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$clea rdi, [rel msg]%1$c%4$c%4$cmov esi, 10%1$c%4$c%4$clea rdx, [rel msg]%1$c%4$c%4$cmov rcx, 34%1$c%4$c%4$cmov r8, 9%1$c%4$c%4$cxor eax, eax%1$c%4$c%4$ccall printf wrt ..plt%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits"
+	msg db "extern printf%1$c%1$csection .data%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$c; Hi there%1$csection .text%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$ccall display   ; Hello :)%1$c%4$c%4$cret%1$c%1$c%4$cdisplay:%1$c%4$c%4$cmov rax, 0%1$c%4$c%4$clea rdi, [rel msg]%1$c%4$c%4$cmov esi, 10%1$c%4$c%4$clea rdx, [rel msg]%1$c%4$c%4$cmov rcx, 34%1$c%4$c%4$cmov r8, 9%1$c%4$c%4$ccall printf wrt ..plt%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits"
 
+; Hi there
 section .text
 	global main
 	main:
+		call display   ; Hello :)
+		ret
+
+	display:
+		mov rax, 0
 		lea rdi, [rel msg]
 		mov esi, 10
 		lea rdx, [rel msg]
 		mov rcx, 34
 		mov r8, 9
-		xor eax, eax
 		call printf wrt ..plt
 		ret
 
