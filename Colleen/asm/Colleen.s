0aac3ec99ddd6f394536cdcd5de3b0eebaa62fd3
Nakusu <nakusu35@hotmail.fr>
2026-03-28 15:06:50 +0100

    [colleen] end asm part

--- Colleen/asm/Colleen.s
+++ Colleen/asm/Colleen.s
@@ -1,19 +1,18 @@
 extern printf
 
 section .data
-    msg db "extern printf", 0
+	msg db "extern printf%1$c%1$csection .data%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$csection .text%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$clea rdi, [rel msg]%1$c%4$c%4$cmov esi, 10%1$c%4$c%4$clea rdx, [rel msg]%1$c%4$c%4$cmov rcx, 34%1$c%4$c%4$cmov r8, 9%1$c%4$c%4$cxor eax, eax%1$c%4$c%4$ccall printf wrt ..plt%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits"
 
 section .text
-global main
+	global main
+	main:
+		lea rdi, [rel msg]
+		mov esi, 10
+		lea rdx, [rel msg]
+		mov rcx, 34
+		mov r8, 9
+		xor eax, eax
+		call printf wrt ..plt
+		ret
 
-main:
-    ;; Specify the number of the system call (1 is `sys_write`).
-    mov rax, 1
-    ;; Set the first argument of `sys_write` to 1 (`stdout`).
-    mov rdi, 1
-    ;; Set the second argument of `sys_write` to the reference of the `msg` variable.
-    mov rsi, msg
-    ;; Set the third argument of `sys_write` to the length of the `msg` variable's value (14 bytes).
-    mov rdx, 9
-    ;; Call the `sys_write` system call.
-    syscall
+section .note.GNU-stack noalloc noexec nowrite progbits
