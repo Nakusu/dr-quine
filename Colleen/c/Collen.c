c17e02c85f9bfe40c283251a8dd565d2ceba64d3
Nakusu <nakusu35@hotmail.fr>
2026-03-28 15:36:33 +0100

    [colleen] fixing tab vs miss

--- Colleen/c/Collen.c
+++ Colleen/c/Collen.c
@@ -3,12 +3,12 @@
 // Hello ;)
 
 char *getContent() {
-	return "#include <stdio.h>%1$c%1$c// Hello ;)%1$c%1$cchar *getContent() {%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint main() {%1$c%2$c// Hi there :)%1$c%1$c    char *content = getContent();%1$c%2$cprintf(content, 10, 9, 34, content);%1$c}";
+	return "#include <stdio.h>%1$c%1$c// Hello ;)%1$c%1$cchar *getContent() {%1$c%2$creturn %3$c%4$s%3$c;%1$c}%1$c%1$cint main() {%1$c%2$c// Hi there :)%1$c%1$c%2$cchar *content = getContent();%1$c%2$cprintf(content, 10, 9, 34, content);%1$c}";
 }
 
 int main() {
 	// Hi there :)
 
-    char *content = getContent();
+	char *content = getContent();
 	printf(content, 10, 9, 34, content);
 }
