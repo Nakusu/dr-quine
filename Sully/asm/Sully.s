48cf1bcfe3d7d8a495bf790a1564663ecf2f5f89
Nakusu <nakusu35@hotmail.fr>
2026-03-29 18:32:28 +0200

    [sully] init asm part

--- Sully/asm/Sully.s
+++ Sully/asm/Sully.s
@@ -1,22 +1,36 @@
 extern dprintf
 extern sprintf
+extern system
 
 section .data
     i dd 5
-	fileformat db "Sully_%d.s", 0
-	msg db "extern dprintf%1$c%1$csection .data%1$c%4$cfilename db %3$cGrace_kid.s%3$c, 0%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$c; Hi there%1$csection .text%1$c%1$c%4$c%%macro openfile 0%1$c%4$c%4$cmov rax, 2%1$c%4$c%4$clea rdi, [rel filename]%1$c%4$c%4$cmov rsi, 577%1$c%4$c%4$cmov rdx, 0644o%1$c%4$c%4$csyscall%1$c%4$c%4$clea rcx, [rel fd]%1$c%4$c%4$cmov [rcx], rax%1$c%4$c%%endmacro%1$c%1$c%4$c%%macro writecontent 0%1$c%4$c%4$cmov rdi, [rel fd]%1$c%4$c%4$clea rsi, [rel msg]%1$c%4$c%4$cmov rdx, 10%1$c%4$c%4$clea rcx, [rel msg]%1$c%4$c%4$cmov r8, 34%1$c%4$c%4$cmov r9, 9%1$c%4$c%4$cmov rax, 0%1$c%4$c%4$csub rsp, 8%1$c%4$c%4$ccall dprintf wrt ..plt%1$c%4$c%4$cadd rsp, 8%1$c%4$c%%endmacro%1$c%1$c%4$c%%macro closefile 0%1$c%4$c%4$cmov rax, 3%1$c%4$c%4$cmov rdi, [rel fd]%1$c%4$c%4$csyscall%1$c%4$c%%endmacro%1$c%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$copenfile%1$c%4$c%4$cwritecontent%1$c%4$c%4$cclosefile%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%1$csection .bss%1$c%4$cfd resq 1%1$c"
+	file_format db "Sully_%d.s", 0
+    cmd_format db "nasm -f elf64 Sully_%1$d.s -o Sully_%1$d.o && gcc Sully_%1$d.o -o Sully_%1$d && ./Sully_%1$d", 0
+	content db "extern dprintf%1$cextern sprintf%1$cextern system%1$c%1$csection .data%1$c	i dd %4$d%1$c	file_format db %3$cSully_%%d.s%3$c, 0%1$c	cmd_format db %3$cnasm -f elf64 Sully_%%1$d.s -o Sully_%%1$d.o && gcc Sully_%%1$d.o -o Sully_%%1$d && ./Sully_%%1$d%3$c, 0%1$c	content db %3$c%2$s%3$c, 0%1$c%1$c; Hi there%1$csection .text%1$c%1$c	exec:%1$c		mov rax, 0%1$c		lea rdi, [rel cmd]%1$c		call system wrt ..plt%1$c		ret%1$c%1$c	format:%1$c		mov rax, 0%1$c		lea rdi, [rel filename]%1$c		lea rsi, [rel file_format]%1$c		mov edx, [rel i]%1$c		call sprintf wrt ..plt%1$c%1$c		mov rax, 0%1$c		lea rdi, [rel cmd]%1$c		lea rsi, [rel cmd_format]%1$c		mov edx, [rel i]%1$c		call sprintf wrt ..plt%1$c		ret%1$c%1$c	openfile:%1$c		mov rax, 2%1$c		lea rdi, [rel filename]%1$c		mov rsi, 577%1$c		mov rdx, 0644o%1$c		syscall%1$c		lea rcx, [rel fd]%1$c		mov [rcx], rax%1$c		ret%1$c%1$c	writecontent:%1$c		mov eax, [rel i]%1$c		sub eax, 1%1$c		mov r9d, eax%1$c%1$c		mov rdi, [rel fd]%1$c		lea rsi, [rel content]%1$c		mov rdx, 10%1$c		lea rcx, [rel content]%1$c		mov r8, 34%1$c		mov rax, 0%1$c		call dprintf wrt ..plt%1$c		ret%1$c%1$c	closefile:%1$c		mov rax, 3%1$c		mov rdi, [rel fd]%1$c		syscall%1$c		ret%1$c%1$c	global main%1$c	main:%1$c		call format%1$c		call openfile%1$c		call writecontent%1$c		call closefile%1$c		call exec%1$c		ret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%1$csection .bss%1$c	filename resb 32%1$c	cmd resb 128%1$c	fd resq 1%1$c", 0
 
 ; Hi there
 section .text
 
-    formatfilename:
+    exec:
+        mov rax, 0
+        lea rdi, [rel cmd]
+        call system wrt ..plt
+        ret
+
+    format:
         mov rax, 0
         lea rdi, [rel filename]
-        lea rsi, [rel fileformat]
+        lea rsi, [rel file_format]
+        mov edx, [rel i]
+        call sprintf wrt ..plt
+        
+        mov rax, 0
+        lea rdi, [rel cmd]
+        lea rsi, [rel cmd_format]
         mov edx, [rel i]
         call sprintf wrt ..plt
         ret
-
+    
 	openfile:
 		mov rax, 2
 		lea rdi, [rel filename]
@@ -28,12 +42,15 @@ section .text
 		ret
 
 	writecontent:
+		mov eax, [rel i]
+		sub eax, 1
+		mov r9d, eax
+
 		mov rdi, [rel fd]
-		lea rsi, [rel msg]
+		lea rsi, [rel content]
 		mov rdx, 10
-		lea rcx, [rel msg]
+		lea rcx, [rel content]
 		mov r8, 34
-		mov r9, 9
 		mov rax, 0
 		call dprintf wrt ..plt
 		ret
@@ -46,14 +63,26 @@ section .text
 
 	global main
 	main:
-        call formatfilename
+		mov eax, [rel i]
+		cmp eax, -1
+		jle .skip_exec
+
+        call format
 		call openfile
 		call writecontent
 		call closefile
+
+		mov eax, [rel i]
+		cmp eax, 0
+		jle .skip_exec
+		call exec
+
+		.skip_exec:
 		ret
 
 section .note.GNU-stack noalloc noexec nowrite progbits
 
 section .bss
     filename resb 32
+    cmd resb 128
 	fd resq 1
