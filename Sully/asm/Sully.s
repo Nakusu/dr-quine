f3f76c80f67a4006f88ff1d9457f7970e5a639c5
Nakusu <nakusu35@hotmail.fr>
2026-03-29 16:56:27 +0200

    [sully] init

---
+++ Sully/asm/Sully.s
@@ -0,0 +1,59 @@
+extern dprintf
+extern sprintf
+
+section .data
+    i dd 5
+	fileformat db "Sully_%d.s", 0
+	msg db "extern dprintf%1$c%1$csection .data%1$c%4$cfilename db %3$cGrace_kid.s%3$c, 0%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$c; Hi there%1$csection .text%1$c%1$c%4$c%%macro openfile 0%1$c%4$c%4$cmov rax, 2%1$c%4$c%4$clea rdi, [rel filename]%1$c%4$c%4$cmov rsi, 577%1$c%4$c%4$cmov rdx, 0644o%1$c%4$c%4$csyscall%1$c%4$c%4$clea rcx, [rel fd]%1$c%4$c%4$cmov [rcx], rax%1$c%4$c%%endmacro%1$c%1$c%4$c%%macro writecontent 0%1$c%4$c%4$cmov rdi, [rel fd]%1$c%4$c%4$clea rsi, [rel msg]%1$c%4$c%4$cmov rdx, 10%1$c%4$c%4$clea rcx, [rel msg]%1$c%4$c%4$cmov r8, 34%1$c%4$c%4$cmov r9, 9%1$c%4$c%4$cmov rax, 0%1$c%4$c%4$csub rsp, 8%1$c%4$c%4$ccall dprintf wrt ..plt%1$c%4$c%4$cadd rsp, 8%1$c%4$c%%endmacro%1$c%1$c%4$c%%macro closefile 0%1$c%4$c%4$cmov rax, 3%1$c%4$c%4$cmov rdi, [rel fd]%1$c%4$c%4$csyscall%1$c%4$c%%endmacro%1$c%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$copenfile%1$c%4$c%4$cwritecontent%1$c%4$c%4$cclosefile%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c%1$csection .bss%1$c%4$cfd resq 1%1$c"
+
+; Hi there
+section .text
+
+    formatfilename:
+        mov rax, 0
+        lea rdi, [rel filename]
+        lea rsi, [rel fileformat]
+        mov edx, [rel i]
+        call sprintf wrt ..plt
+        ret
+
+	openfile:
+		mov rax, 2
+		lea rdi, [rel filename]
+		mov rsi, 577
+		mov rdx, 0644o
+		syscall
+		lea rcx, [rel fd]
+		mov [rcx], rax
+		ret
+
+	writecontent:
+		mov rdi, [rel fd]
+		lea rsi, [rel msg]
+		mov rdx, 10
+		lea rcx, [rel msg]
+		mov r8, 34
+		mov r9, 9
+		mov rax, 0
+		call dprintf wrt ..plt
+		ret
+
+	closefile:
+		mov rax, 3
+		mov rdi, [rel fd]
+		syscall
+		ret
+
+	global main
+	main:
+        call formatfilename
+		call openfile
+		call writecontent
+		call closefile
+		ret
+
+section .note.GNU-stack noalloc noexec nowrite progbits
+
+section .bss
+    filename resb 32
+	fd resq 1
