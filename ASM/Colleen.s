adf8d41de57844d426c6d731005e453b7dd14482
Leopold LEPAGE <llepage@f6r5s3.clusters.42paris.fr>
2026-04-06 10:37:26 +0200

    [ASM] remove unused section on fedora computer

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,7 +1,7 @@
 extern printf
 
 section .data
-	msg db "extern printf%1$c%1$csection .data%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$c; Hi there%1$csection .text%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$ccall display   ; Hello :)%1$c%4$c%4$cret%1$c%1$c%4$cdisplay:%1$c%4$c%4$cmov rax, 0%1$c%4$c%4$clea rdi, [rel msg]%1$c%4$c%4$cmov esi, 10%1$c%4$c%4$clea rdx, [rel msg]%1$c%4$c%4$cmov rcx, 34%1$c%4$c%4$cmov r8, 9%1$c%4$c%4$ccall printf wrt ..plt%1$c%4$c%4$cret%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits"
+	msg db "extern printf%1$c%1$csection .data%1$c%4$cmsg db %3$c%2$s%3$c%1$c%1$c; Hi there%1$csection .text%1$c%4$cglobal main%1$c%4$cmain:%1$c%4$c%4$ccall display   ; Hello :)%1$c%4$c%4$cret%1$c%1$c%4$cdisplay:%1$c%4$c%4$cmov rax, 0%1$c%4$c%4$clea rdi, [rel msg]%1$c%4$c%4$cmov esi, 10%1$c%4$c%4$clea rdx, [rel msg]%1$c%4$c%4$cmov rcx, 34%1$c%4$c%4$cmov r8, 9%1$c%4$c%4$ccall printf wrt ..plt%1$c%4$c%4$cret"
 
 ; Hi there
 section .text
@@ -19,5 +19,3 @@ section .text
 		mov r8, 9
 		call printf wrt ..plt
 		ret
-
-section .note.GNU-stack noalloc noexec nowrite progbits
